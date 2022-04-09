2542b896e3ef45a8ed659a56160520bed4dd2194
SaifSushi <alushisaif@gmail.com>
2022-04-09 18:40:01 -0500

    switched to pg-promise library and testing friend search routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,10 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", db.getUsers)
+
+app.get("/getusers", db.getAllUsers)
+app.get("/getuserbyid/:id", db.getUserById)
+app.get("getuserbyusername/:username", db.getUserByUsername)
 
 app.listen(3000, () => {
     console.log('The application is listening on port 3000!');
--- backend/src/queries.ts
+++ backend/src/queries.ts
@@ -1,28 +1,38 @@
-const {Client} = require("pg")
+const pgp = require('pg-promise')(/* options */)
+// const db = pgp(`postgres://${process.env.user}:${process.env.password}@${process.env.host}:${process.env.port}/${process.env.database}`)
+const db = pgp('postgres://postgres:rootuser@localhost:5432/mack')
+import { Request, Response } from "express"
 require('dotenv').config()
-import { Request, Response} from "express"
 
-const client = new Client({
-    host: process.env.host,
-    user: process.env.user,
-    port: process.env.port,
-    password: process.env.password,
-    database: process.env.database
-})
-
-            
-const getUsers = (req: Request, res: Response) => {
-client.connect()
-client.query('SELECT * FROM users ORDER BY user_id ASC', (error: any, results: { rows: any }) => {
-      if (error) {
-        throw error
-      }
-      res.status(200).json(results.rows)
-      client.end
-    })
+const getAllUsers = async (req: Request, res: Response) => {
+    try{
+        const users = await db.any("SELECT * FROM users")
+        res.status(200).json(users)
+    } catch(err) {
+        throw err
+    }
 }
 
+const getUserById = async (req: Request, res: Response) => {
+    try{
+        const userById = await db.one("SELECT * FROM users WHERE user_id = $1", [req.params.id])
+        res.status(200).json(userById)
+    } catch(err){
+        throw err
+    }
+}
+
+const getUserByUsername = async (req: Request, res: Response) => {
+    try{
+        const userByUsername = await db.any("SELECT user_id, username, email FROM users WHERE username LIKE $1", [`${req.params.username}%`])
+        res.status(200).json(userByUsername)
+    } catch(err) {
+        throw err
+    }
+}
 
 module.exports = {
-    getUsers
+    getAllUsers,
+    getUserById,
+    getUserByUsername
 }
