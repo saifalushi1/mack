b07c60042bf7174763241dba495cc4b20205c78e
SaifSushi <alushisaif@gmail.com>
2022-04-18 03:00:57 -0500

    fixed some mesasge routes and added new ones

--- backend/src/controllers/messageController.ts
+++ backend/src/controllers/messageController.ts
@@ -2,7 +2,9 @@ const messagedb = require("./messageQueries")
 import express from "express"
 const router = express.Router()
 
-router.post("/create", messagedb.sendMessage)
-
+router.get("/lastReceived/:id", messagedb.getLastTenMessagesReceived)
+router.get("/lastSent/:id", messagedb.getLastTenMessagesSent)
+router.get("/test", messagedb.getAllMessagesFromUser)
+router.post("/create/:creatorId/:recipient_id", messagedb.sendMessage)
 
 module.exports = router
