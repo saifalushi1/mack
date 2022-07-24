fc50a598d54db01f5ed3cd1465a72c9dfbd40622
SaifSushi <alushisaif@gmail.com>
2022-07-24 13:23:22 -0500

    working on signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,12 +27,16 @@ const App = () => {
 
     return (
         <>
-            <Navigation />
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="chat" element={<Chat user={user} connected={connected} />} />
-                <Route path="/signup" element={<SignUp />} />
-            </Routes>
+            <div className="flex items-center justify-center h-screen min-h-full px-4 py-12 sm:px-6 lg:px-8">
+                <div className="w-full max-w-md space-y-8">
+                    <Navigation />
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/signup" element={<SignUp />} />
+                    </Routes>
+                </div>
+            </div>
+            <Route path="chat" element={<Chat user={user} connected={connected} />} />
         </>
     )
 }
