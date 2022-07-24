2d8f1f2d439fae1916c16ac957e3bfe0266c4039
SaifSushi <alushisaif@gmail.com>
2022-07-24 13:31:03 -0500

    added some components

---
+++ frontend/src/components/login/LoginPage.tsx
@@ -0,0 +1,11 @@
+
+const LoginPage = () => {
+    return (
+        <>
+                <div className="flex items-center justify-center h-screen min-h-full px-4 py-12 sm:px-6 lg:px-8"></div>
+                <div className="w-full max-w-md space-y-8"></div>
+        </>
+    )
+}
+
+export default LoginPage
