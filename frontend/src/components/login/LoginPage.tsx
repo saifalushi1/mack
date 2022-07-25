3b6e2b0b523fb42ce237d9f00d6bc035d4a395f1
SaifSushi <alushisaif@gmail.com>
2022-07-25 01:01:35 -0500

    login html/css done. Need to add logic and finish signup/forgot-pass

--- frontend/src/components/login/LoginPage.tsx
+++ frontend/src/components/login/LoginPage.tsx
@@ -37,6 +37,36 @@ const LoginPage = () => {
                             ></input>
                         </div>
                     </div>
+                    <div className="flex items-center justify-between ">
+                        <div className="flex items-center">
+                            <input
+                                id="remember-me"
+                                name="remember-me"
+                                type="checkbox"
+                                className="w-4 h-4 text-purple-600 border-gray-300 rounded focus:ring-purple-500"
+                            />
+                            <label
+                                htmlFor="remember-me"
+                                className="block ml-2 text-sm text-gray-900"
+                            >
+                                Remember me
+                            </label>
+                        </div>
+                        <div className="text-sm">
+                            <a
+                                href="#"
+                                className="font-medium text-purple-600 hover:text-purple-500"
+                            >
+                                Forgot your password?
+                            </a>
+                        </div>
+                    </div>
+                    <button
+                        className="relative flex justify-center w-full px-4 py-2 mt-10 text-sm font-medium text-white bg-purple-600 border border-transparent rounded-md group hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+                        onClick={() => ""}
+                    >
+                        Login
+                    </button>
                 </div>
             </div>
         </>
